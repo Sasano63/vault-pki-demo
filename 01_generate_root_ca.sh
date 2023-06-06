d4703319b5c2efa2aaac486374f4bdd3ddfbb331
Sasano63 <sasano@hashicorp.com>
2023-06-06 08:43:52 +0200

    fixed typo

--- 01_generate_root_ca.sh
+++ 01_generate_root_ca.sh
@@ -10,7 +10,7 @@ vault secrets tune -max-lease-ttl=8760h pki
 # generate root certificate
 vault write -field=certificate pki/root/generate/internal \
      common_name="example.com" \
-     ttl=87600h > CA_cert.crt
+     ttl=8760h > CA_cert.crt
      
 #url setzen
 vault write pki/config/urls \
